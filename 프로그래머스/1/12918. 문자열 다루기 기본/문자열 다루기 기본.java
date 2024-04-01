e5b2f055424e3f5b3b738cbaf53c95d2461ff149
장수빈 <83440978+wkdtnqls0506@users.noreply.github.com>
2024-04-01 13:06:23 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.03 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean solution(String s) {
+        boolean answer = false;
+        char[] arr = s.toCharArray();
+        if (s.length() == 4 || s.length() == 6) {
+            for (int i = 0; i < arr.length; i++) {
+                if (Character.getNumericValue(arr[i]) > 9) {
+                    answer = false;
+                    break;
+                }
+                else answer = true;
+            }   
+        }
+        return answer;
+    }
+}
