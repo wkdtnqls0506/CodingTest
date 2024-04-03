944f350d2cb3b9660a15b9c389e37eb6e6592e0a
장수빈 <83440978+wkdtnqls0506@users.noreply.github.com>
2024-04-03 13:45:26 +0900

    [level 1] Title: 예산, Time: 0.49 ms, Memory: 81.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        int cnt = 0;
+        Arrays.sort(d);
+        for (int i = 0; i < d.length; i++) {
+            answer += d[i]; 
+            if (answer <= budget) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
