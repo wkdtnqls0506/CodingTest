2b6278c8ef81f38ab2a3f250e56e2b3b209ee519
장수빈 <83440978+wkdtnqls0506@users.noreply.github.com>
2024-05-12 21:30:31 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.07 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int k, int[] score) {
+        int[] answer = new int[score.length];
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < score.length; i++) {
+            list.add(score[i]);
+            if(list.size() > k) list.remove(Collections.min(list));
+            answer[i] = Collections.min(list);
+        }
+        return answer;
+    }
+}
