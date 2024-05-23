3924f7d9d11d3025b2fcc8891a7e418f4d93cdb1
장수빈 <83440978+wkdtnqls0506@users.noreply.github.com>
2024-05-23 21:32:21 +0900

    [level 1] Title: 폰켓몬, Time: 2.45 ms, Memory: 87.4 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,8 +2,10 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        Integer[] numsArray = Arrays.stream(nums).boxed().toArray(Integer[]::new);
-        Set<Integer> set = new HashSet(Arrays.asList(numsArray));
+        Set<Integer> set = new HashSet();
+        for (int i = 0; i < nums.length; i++) {
+            set.add(nums[i]);
+        }
         return set.size() > nums.length / 2 ? nums.length / 2 : set.size();
     }
 }
