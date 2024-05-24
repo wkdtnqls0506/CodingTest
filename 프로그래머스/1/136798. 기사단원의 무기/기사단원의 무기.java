5222bdcba081bbe0c0a6f4ba00dc90ecad8406ee
장수빈 <83440978+wkdtnqls0506@users.noreply.github.com>
2024-05-24 20:27:07 +0900

    [level 1] Title: 기사단원의 무기, Time: 0.73 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -1,15 +1,13 @@
 class Solution {
     public int solution(int number, int limit, int power) {
-        int[] size = new int[number + 1];
-        for (int i = 1; i <= number; i++) {
-            for (int j = 1; j <= number / i; j++) {
-                size[i * j]++;
-            }
-        }
         int answer = 0;
         for (int i = 1; i <= number; i++) {
-            if(size[i] > limit) answer += power;
-            else answer += size[i];
+            int count = 0;
+            for (int j = 1; j * j <= i; j++) {
+                if (j * j == i) count++;
+                else if (i % j == 0) count += 2;
+            }
+            answer += count > limit ? power : count;      
         }
         return answer;
     }
