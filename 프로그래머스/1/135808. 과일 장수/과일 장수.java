08e26c54507435c22485cd1c727ea7ff442d29dc
장수빈 <83440978+wkdtnqls0506@users.noreply.github.com>
2024-06-01 16:58:16 +0900

    [level 1] Title: 과일 장수, Time: 0.48 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -3,12 +3,9 @@ import java.util.*;
 class Solution {
     public int solution(int k, int m, int[] score) {
         int answer = 0;
-        
-        Integer[] list = Arrays.stream(score).boxed().toArray(Integer[]::new);
-        Arrays.sort(list, Collections.reverseOrder());
-        
-        for(int i = 0; i < list.length; i++) {
-            if((i + 1) % m == 0) answer += (list[i] * m);
+        Arrays.sort(score);
+        for(int i = score.length; i >= m; i -= m){
+            answer += score[i - m] * m;
         }
         return answer;
     }
