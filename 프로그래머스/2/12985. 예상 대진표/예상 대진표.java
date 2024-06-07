a106d1b438afe1d368b2be24455c7752594811b7
장수빈 <83440978+wkdtnqls0506@users.noreply.github.com>
2024-06-07 15:11:58 +0900

    [level 2] Title: 예상 대진표, Time: 0.04 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -1,18 +1,11 @@
-class Solution
-{
-    public int solution(int n, int a, int b)
-    {
+class Solution {
+    public int solution(int n, int a, int b) {
         int answer = 0;
-        while(Math.abs(a - b) >= 1) {
-            if(a % 2 == 0) a /= 2;
-            else if(a % 2 != 0) a = (a + 1) / 2;
-            
-            if(b % 2 == 0) b/= 2;
-            else if(b % 2 != 0) b = (b + 1) / 2;
-            
+        while (a != b) {
+            a = (a + 1) / 2;
+            b = (b + 1) / 2;
             answer++;
         }
-
         return answer;
     }
 }
