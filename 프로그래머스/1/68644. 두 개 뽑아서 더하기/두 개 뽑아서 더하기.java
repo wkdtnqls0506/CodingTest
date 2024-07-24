e3c0ae950e0be16ff0e66ae744eee763fcd73a65
장수빈 <83440978+wkdtnqls0506@users.noreply.github.com>
2024-07-25 00:34:35 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 10.59 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -2,14 +2,12 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] numbers) {
-        ArrayList<Integer> arrList = new ArrayList();
+        HashSet<Integer> set = new HashSet<>();
         for (int i = 0; i < numbers.length; i++) {
-            for (int j = i+1; j < numbers.length; j++) {
-                arrList.add(numbers[i] + numbers[j]);
+            for(int j = i + 1; j < numbers.length; j++) {
+                set.add(numbers[i] + numbers[j]);
             }
         }
-        numbers = arrList.stream().mapToInt(x->x).distinct().toArray();
-        Arrays.sort(numbers);
-        return numbers;
+        return set.stream().sorted().mapToInt(Integer::intValue).toArray();
     }
 }
