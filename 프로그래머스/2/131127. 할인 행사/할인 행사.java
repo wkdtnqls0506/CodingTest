f7728b86d0079f33886dfe48e6ea47f7a17e1f39
장수빈 <83440978+wkdtnqls0506@users.noreply.github.com>
2024-09-19 13:36:17 +0900

    [level 2] Title: 할인 행사, Time: 116.51 ms, Memory: 121 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -3,30 +3,21 @@ import java.util.*;
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
-        Map<String, Integer> wantMap = new HashMap<>();
+        HashMap<String, Integer> wantMap = new HashMap<>();
         
-        for(int i = 0; i < want.length; i++) {
+        for (int i = 0; i < want.length; i++) {
             wantMap.put(want[i], number[i]);
         }
         
-        for (int i = 0; i <= discount.length - 10; i++) {
-            String[] arr1 = Arrays.copyOfRange(discount, i, i + 10); 
-            Map<String, Integer> discountMap = new HashMap<>();
+        for (int i = 0; i < discount.length - 9; i++) {
+            HashMap<String, Integer> discountMap = new HashMap<>();
             
-            for (int j = 0; j < arr1.length; j++) {
-                discountMap.put(arr1[j], discountMap.getOrDefault(arr1[j], 0) + 1);
+            for (int j = i; j < i + 10; j++) {
+                discountMap.put(discount[j], discountMap.getOrDefault(discount[j], 0) + 1);
             }
             
-            boolean match = true;
+            if(wantMap.equals(discountMap)) answer++;
             
-            for(String key : wantMap.keySet()) {
-                if(wantMap.get(key) != discountMap.get(key)) {
-                    match = false;
-                    break;
-                }
-            }
-            
-            if(match) answer++;
         }
         return answer;
     }
